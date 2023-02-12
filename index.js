020edef971d7c5bd6180c665a6fa7306ba641d83
Tushargandhi07 <tushargan7@gmail.com>
2023-02-12 19:06:10 +0530

    navbar responsive done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 GitHubCalendar(".calendar", "Tushargandhi07", { responsive: true });
+
+const menu= document.querySelector('.menu');
+const openMenu= document.querySelector('.openMenu');
+const closeMenu= document.querySelector('.closeMenu');
+
+
+openMenu.addEventListener('click',showm)
+closeMenu.addEventListener('click',closem)
+
+function showm(){
+    menu.style.display='flex';
+    menu.style.top='0';
+}
+
+function closem(){
+    menu.style.top='-100%';
+    console.log("yes")
+}
