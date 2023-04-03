63d0714822468fc21e4654fe80463fb8d5d99f8b
Tushargandhi07 <tushargan7@gmail.com>
2023-04-03 22:56:44 +0530

    nxm-301 project added

--- index.js
+++ index.js
@@ -34,9 +34,27 @@ var auto = new Typed("#auto_type", {
 // reveal elements
 
 window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal2);
 
 function reveal() {
     var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight= window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint=150;
+
+        if(revealtop<windowheight-revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+function reveal2() {
+    var reveals = document.querySelectorAll('.reveal2');
 
     for (var i = 0; i < reveals.length; i++) {
         var windowheight= window.innerHeight;
